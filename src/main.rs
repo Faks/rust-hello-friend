32a4a2aef2f8fc4808ca8ce52ea19cbc7edf8dae
Oskars Germovs <oskars_germovs@inbox.lv>
2024-09-16 00:12:04 +0300

    Hello, freiend.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::process::exit;
 
 fn main() {
-    println!(" Hello, friend. Hello, friend? That’s lame. Maybe I should give you a name, but that’s a slippery slope. You’re only in my head. We have to remember that. Shit.");
-    exit(0);
+    println!("Hello, friend. Hello, friend? That’s lame. Maybe I should give you a name, but that’s a slippery slope. You’re only in my head. We have to remember that. Shit.");
 }
